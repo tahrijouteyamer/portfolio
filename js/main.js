7dfb093f98b00205f0f005eb3a4025a31141f79a
Amer Tahri-Joutey <125614736+tahrijouteyamer@users.noreply.github.com>
2024-10-24 22:53:56 +0100

    Update

--- js/main.js
+++ js/main.js
@@ -20,11 +20,10 @@ function getLanguage() {
 
 const language = getLanguage();
 
-// Update title based on language
+
 const blurTitle = titles[language].blur;
 const focusTitle = titles[language].focus;
 
-// Change title when tab is inactive
 window.addEventListener('blur', () => {
   document.title = blurTitle;
 });
@@ -82,11 +81,10 @@ document.addEventListener('scroll', function() {
   const descriptionTop = document.querySelector('.about__description').getBoundingClientRect().top + window.scrollY;
   const scrollOffset = scrollPosition - descriptionTop;
 
-  // Define how much scrolling it takes for a word to be fully visible
-  const maxScrollForEffect = 300; // Adjust this value to control scroll sensitivity
 
+  const maxScrollForEffect = 300; 
   descriptionWords.forEach((word, index) => {
-    const wordPosition = index * 3; // Adjust the multiplier to control the stagger effect
+    const wordPosition = index * 3;
     const opacity = Math.min((scrollOffset - wordPosition) / maxScrollForEffect, 2);
 
     word.style.opacity = opacity > 0 ? opacity : 0;
@@ -121,19 +119,15 @@ const themeButton = document.getElementById("theme-button");
 const lightTheme = "light-theme";
 const iconTheme = "bx-sun";
 
-// Previously selected topic (if user selected)
 const selectedTheme = localStorage.getItem("selected-theme");
 const selectedIcon = localStorage.getItem("selected-icon");
 
-// We obtain the current theme that the interface has by validating the light-theme class
 const getCurrentTheme = () =>
   document.body.classList.contains(lightTheme) ? "dark" : "light";
 const getCurrentIcon = () =>
   themeButton.classList.contains(iconTheme) ? "bx bx-moon" : "bx bx-sun";
 
-// We validate if the user previously chose a topic
 if (selectedTheme) {
-  // If the validation is fulfilled, we ask what the issue was to know if we activated or deactivated the light
   document.body.classList[selectedTheme === "dark" ? "add" : "remove"](
     lightTheme
   );
@@ -142,75 +136,11 @@ if (selectedTheme) {
   );
 }
 
-// Activate / deactivate the theme manually with the button
 themeButton.addEventListener("click", () => {
   // Add or remove the light / icon theme
   document.body.classList.toggle(lightTheme);
   themeButton.classList.toggle(iconTheme);
-  // We save the theme and the current icon that the user chose
   localStorage.setItem("selected-theme", getCurrentTheme());
   localStorage.setItem("selected-icon", getCurrentIcon());
 });
-
-/*Scroll Reveal Animation*/
-const sr = ScrollReveal({
-  origin: "top",
-  distance: "60px",
-  duration: 2500,
-  delay: 400,
-  reset: true,
-});
-
-sr.reveal(`.nav__menu`, {
-  delay: 100,
-  scale: 0.1,
-  origin: "bottom",
-  distance: "300px",
-});
-
-sr.reveal(`.home__data`);
-sr.reveal(`.home__handle`, {
-  delay: 100,
-});
-
-sr.reveal(`.home__social, .home__scroll`, {
-  delay: 100,
-  origin: "bottom",
-});
-
-sr.reveal(`.services__title, services__button`, {
-  delay: 100,
-  scale: 0.9,
-  origin: "top",
-  distance: "30px",
-});
-
-sr.reveal(`.testimonial__container`, {
-  delay: 100,
-  scale: 0.9,
-  origin: "bottom",
-  distance: "30px",
-});
-
-sr.reveal(`.contact__info, .contact__title-info`, {
-  delay: 100,
-  scale: 0.9,
-  origin: "left",
-  distance: "30px",
-});
-
-sr.reveal(`.contact__form, .contact__title-form`, {
-  delay: 100,
-  scale: 0.9,
-  origin: "right",
-  distance: "30px",
-});
-
-sr.reveal(`.footer, footer__container`, {
-  delay: 100,
-  scale: 0.9,
-  origin: "bottom",
-  distance: "30px",
-});
-
 // Full code might be added later
